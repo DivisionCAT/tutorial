c7bf2f35400c29b441a974b9054c4946a6e8d3dd
P4rkJW <wr4gcmoima@naver.com>
2019-09-19 15:07:55 +0900

    react.js - add props and state

--- React/P4rkJW_project_1/src/App.js
+++ React/P4rkJW_project_1/src/App.js
@@ -2,30 +2,42 @@ import React, { Component } from 'react';
 import './css/email_auth.css';
 import reactDOM from 'react-dom'
 
+
 class App extends Component{
+    static defaultProps = {
+        title : "P4rkJW Email Auth"
+    };
+
+    state = {
+        content : "Input Your ID/PW",
+        display : 'none'
+    };
+
+    AuthEmail(){
+        this.setState({
+            content: "Auth Your E-mail",
+            display : 'block'
+        });
+    }
+
   render()
   {
     return (
         <div className="email">
         <form className="login-form">
           <p className="login-text">
-            <h1>Hello~</h1>
+              <h2>{this.props.title}</h2>
+            <h6>{this.state.content}</h6>
           </p>
           <input type="email" className="login-username" autoFocus="true" required="true" placeholder="Email"/>
           <input type="password" className="login-password" required="true" placeholder="Password"/>
-          <input type="submit" name="Login" value="Login" className="login-submit"/>
+            <div style= {{display: `${ this.state.display }`}}><input type="text"  className="login-password" placeholder="Input Your Auth Code"/></div>
+          <input type="submit" name="Login" value="Login" onClick={this.AuthEmail.bind(this)} className="login-submit"/>
         </form>
         </div>
     );
   }
 }
 
-function createMarkup(){
-    return {__html : 'a'};
-}
-
-function MyComponent(){
-    return <div dangerouslySetInnerHTML={createMarkup()} />;
-}
 
 export default App;
